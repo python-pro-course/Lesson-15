2aba85507d30c5c4a0d0f7ad7035638322ab93f4
mokson07 <116485200+mokson07@users.noreply.github.com>
2022-11-06 11:09:57 +0300

    Add files via upload

---
+++ Lesson-15/main.py
@@ -0,0 +1,11 @@
+class chel:
+    def name(self):
+        print('vasa')
+    def ago(self):
+        print('77')
+    def familia(self):
+        print('chel')
+    def gl(self):
+        print('goluboy')
+    def color(self):
+        print('black')
