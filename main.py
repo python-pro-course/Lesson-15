66f9c77a03362d43a8cb420e66e8466f26f9d8db
Maksim Chernov <chernov.maksim2008@gmail.com>
2022-11-06 11:07:59 +0300

    pon v1

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class chel:
+    def name(self):
+        print('vasa')
+    def ago(self):
+        print('77')
+    def familia(self):
+        print('chel')
+    def gl(self):
+        print('goluboy')
+    def color(self):
+        print('black')
